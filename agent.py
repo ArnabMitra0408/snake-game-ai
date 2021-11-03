68f8d8a56269e6c9cb8934e34227333f408e8ae8
ArnabMitra0408 <arnabmitra490@gmail.com>
2021-11-03 19:44:12 +0530

    final agent

--- agent.py
+++ agent.py
@@ -135,11 +135,7 @@ def train():
 
             print('Game', agent.n_games, 'Score', score, 'Record:', record)
 
-            plot_scores.append(score)
-            total_score += score
-            mean_score = total_score / agent.n_games
-            plot_mean_scores.append(mean_score)
-            plot(plot_scores, plot_mean_scores)
+            
             if (agent.n_games>2500):
             	break
 
